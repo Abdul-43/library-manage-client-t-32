e12b48035262a2500c24c6078450e778f683a1ef
Abdul Rahman <64832120+Abdul-43@users.noreply.github.com>
2023-05-12 17:45:54 +0530

    Update BookForm.js

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -6,32 +6,41 @@ import { Button } from 'react-bootstrap';
 const validationSchema = Yup.object().shape({
   title: Yup.string().required('Title is required'),
   author: Yup.string().required('Author is required'),
-  // Add more validation rules as needed
 });
 
 const BookForm = ({ initialValues, onSubmit }) => {
+
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={onSubmit}
-    >
-      <Form>
-        <div className="form-group">
-          <label htmlFor="title" className="form-label">Title:</label>
-          <Field type="text" id="title" name="title" className="form-control" />
-          <ErrorMessage name="title" component="div" className="error" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="author" className="form-label">Author:</label>
-          <Field type="text" id="author" name="author" className="form-control" />
-          <ErrorMessage name="author" component="div" className="error" />
-        </div>
-        {/* Add more form fields as needed */}
-        <Button type="submit" variant="primary">Submit</Button>
-      </Form>
-    </Formik>
+    <div>
+      <h2>{initialValues ? 'Add Book' : 'Edit Book'}</h2>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form>
+          <div className="form-group">
+            <label htmlFor="title" className="form-label">
+              Title:
+            </label>
+            <Field type="text" id="title" name="title" className="form-control" />
+            <ErrorMessage name="title" component="div" className="error" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="author" className="form-label">
+              Author:
+            </label>
+            <Field type="text" id="author" name="author" className="form-control" />
+            <ErrorMessage name="author" component="div" className="error" />
+          </div>
+          <Button type="submit" variant="primary">
+            Submit
+          </Button>
+        </Form>
+      </Formik>
+    </div>
   );
 };
 
 export default BookForm;
+
